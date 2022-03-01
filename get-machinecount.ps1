ce471dd6b883b9787677dd126f4c72af6f9e44da
DAS <46620390+wizhardhacker1@users.noreply.github.com>
2022-03-01 15:56:34 -0800

    Add files via upload

---
+++ get-machinecount.ps1
@@ -0,0 +1,7 @@
+﻿$Computers = (Get-ADComputer -Filter *).count
+$Workstations = (Get-ADComputer -LDAPFilter "(&(objectClass=Computer)(!operatingSystem=*server*))" -Searchbase (Get-ADDomain).distinguishedName).count
+$Servers = (Get-ADComputer -LDAPFilter "(&(objectClass=Computer)(operatingSystem=*server*))" -Searchbase (Get-ADDomain).distinguishedName).count
+
+Write-Host "Computers =      "$Computers
+Write-Host "Workstions =     "$Workstations
+Write-Host "Servers =        "$Servers 
---
+++ lastLoginfromfile.ps1
@@ -0,0 +1,7 @@
+﻿ $file = Get-Content -Path "C:\BGinfo\users.txt"
+ foreach ($user in $file)
+     {
+     Get-ADUser -Identity $user -Properties LastLogonDate | Select-Object SamAccountName, LastLogonDate
+     }
+
+
